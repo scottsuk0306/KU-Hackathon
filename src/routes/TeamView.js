82b2d7591b423eb28fe594b0077eb994b503acb9
jaylions <jaylions@snu.ac.kr>
2022-08-20 15:24:09 +0900

    firebase랑 연결

--- src/routes/TeamView.js
+++ src/routes/TeamView.js
@@ -1,10 +1,14 @@
 import React, { useState,useEffect } from "react";
 import { getAuth,signOut,updateProfile } from "firebase/auth";
 import { useHistory } from "react-router-dom";
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { addDoc ,collection} from "firebase/firestore";
+import { async } from "@firebase/util";
 const TeamView = ({userObj})=>{
     const [goal,setGoal]=useState("");
     const [description,setDescription]=useState("");
+    const [num,setNum]=useState(0);
+    const [date,setDate]=useState(Date.now());
     const onChangeGoal =(event)=>{
         const {
             target:{value},
@@ -17,16 +21,41 @@ const TeamView = ({userObj})=>{
         }=event;
         setDescription(value);
     }
+    const onChangeNum=(event)=>{
+        const{
+            target:{value},
+        }=event;
+        setNum(value);
+    }
+    const onChangeDate=(event)=>{
+        const{
+            target:{value},
+        }=event;
+        setDate(value);
+    }
+    const onSubmit=async(event)=>{
+        event.preventDefault();
+        const newTeam={
+            goal:goal,
+            dueDate:date,
+            number:num,
+            description:description,
+            creatorId:userObj.uid,
+        }
+        await addDoc(collection(dbService,"teamlist"),newTeam);
+        console.log(newTeam)
+        };
     
+
     return(
         <>
             <h3>팀을 생성해보세요</h3>
-            <form >
+            <form onSubmit={onSubmit} >
                 팀의 목표<br></br><input type="text" value={goal} onChange={onChangeGoal}/><br/>
-                마감 날짜<br></br><input type="date"/><br></br>
-                모집 인원<br></br><input type="number" /><br/>
+                마감 날짜<br></br><input type="date" value={date} onChange={onChangeDate}/><br></br>
+                모집 인원<br></br><input type="number" value={num} onChange={onChangeNum}/><br/>
                 상세 설명<br></br><input type="text" value={description} onChange={onChangeDescription}/><br></br>
-                <input type="submit" value="팀 생성하기"/> 
+                <input type="submit" value="팀 생성하기" /> 
             </form>
                        
         </>
