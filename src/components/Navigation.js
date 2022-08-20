72eaecc4de1517e4521a496bba668f27a2acd4c2
scott <scottsuk0306@kaist.ac.kr>
2022-08-20 15:53:59 +0900

    Add navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const Navigatin=({userObj})=><nav>
-    <ul>
-        <li>
-        <Link to="/">Home</Link>
-        </li>
-        <li>
-        <Link to="/profile">{userObj.displayName}의 profile</Link>
-        </li>
-        <li>
-        <Link to="/teamview">팀 모집하기</Link>
-        </li>
-    </ul>
-</nav>;
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons'
 
-export default Navigatin;
+const Navigation = ({ userObj }) => (
+  <nav>
+    <ul style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
+      <li>
+        <Link to='/' style={{ marginRight: 10 }}>
+          <FontAwesomeIcon icon={faTwitter} color={'#04AAFF'} size='2x' />
+        </Link>
+      </li>
+      <li>
+        <Link
+          to='/profile'
+          style={{
+            marginLeft: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontSize: 12
+          }}
+        >
+          <FontAwesomeIcon icon={faUser} color={'#04AAFF'} size='2x' />
+          <span style={{ marginTop: 10 }}>
+            {userObj.displayName
+              ? `${userObj.displayName}의 Profile`
+              : 'Profile'}
+          </span>
+        </Link>
+      </li>
+      <li>
+        <Link
+          to='/teamview'
+          style={{
+            marginLeft: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontSize: 12
+          }}
+        >
+          <FontAwesomeIcon icon={faUsers} color={'#04AAFF'} size='2x' />
+          <span style={{ marginTop: 10 }}>
+            {userObj.displayName
+              ? `${userObj.displayName}의 Profile`
+              : 'Profile'}
+          </span>
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+export default Navigation
